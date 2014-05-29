b8a144d1287a5515ead895ede93c10df0445fd88
Sebastian Sierra <sebastian@tarmac.io>
2014-05-29 17:26:44 -0300

    Changes for deploy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ http.createServer(function (req, res) {
       res.end();
   });
 
-}).listen(1337, '127.0.0.1');
+}).listen(process.env.PORT);
 
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running ...');
 
